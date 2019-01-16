93d6cdc3f4c53278a9d445f2a3512dab7f24cb5c
Ashwatth Nagpal <ashwatthnagpal@Ashwatths-MacBook-Pro-2.local>
2019-01-16 10:31:04 +0530

    throw error for partially invalid input string

--- rna-transcription.spec.js
+++ rna-transcription.spec.js
@@ -41,7 +41,7 @@ describe('Transcriptor', () => {
       .toThrow(new Error('Invalid input DNA.'));
   });
 
-  xtest('correctly handles partially invalid input', () => {
+  test('correctly handles partially invalid input', () => {
     expect(() => toRna('ACGTXXXCTTAA'))
       .toThrow(new Error('Invalid input DNA.'));
   });
